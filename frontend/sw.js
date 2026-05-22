dcac230821b0ebb8b43e4111cbfc7b16166834a3
quetzitaxd <quetzitaxd@gmail.com>
2026-05-21 22:25:43 -0600

    feat: implementa logo.png y favicon.ico en todo el sitio
    - Copia logo.png de img/ a assets/brand/
    - Copia favicon.ico de img/ a assets/brand/
    - Actualiza favicon en todas las paginas a /assets/brand/favicon.ico
    - Actualiza apple-touch-icon a /assets/brand/logo.png
    - Actualiza manifest.json shortcuts (Tienda -> /, Ofertas -> /?filter=sale)
    - Actualiza sw.js para cachear logo.png y favicon.ico
    - Muestra logo.png en instalar.html

--- frontend/sw.js
+++ frontend/sw.js
@@ -32,7 +32,8 @@ const STATIC_ASSETS = [
   '/manifest.json',
   '/assets/icons/icon-192x192.png',
   '/assets/icons/icon-512x512.png',
-  '/assets/brand/logo-icon.svg',
+  '/assets/brand/logo.png',
+  '/assets/brand/favicon.ico',
   'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css'
 ];
 
